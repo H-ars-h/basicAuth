b08a8b4d8897598df2721c6b6a453c4033e74321
H-ars-h <harshtandekar80@gmail.com>
2021-07-03 01:46:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express =  require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const db = require('./config/keys').MongoURI;
+
+mongoose.connect(db, { userNameUrlParse: true})
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false}));
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server start on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://harsh123:harsh123@cluster0.mmjpk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
